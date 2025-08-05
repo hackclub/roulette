931640a64a2cc3a0940cdade6fcae71b5aea8f06
bucketfishy <bucketfishy@gmail.com>
2025-08-04 21:04:09 -0700

    ogimg again?

--- app/layout.js
+++ app/layout.js
@@ -11,33 +11,17 @@ const righteous = Righteous({
 export const metadata = {
   title: "Gamedev Roulette",
   description: "Spin wheels. Make games. Get prizes.",
+  openGraph: {
+    title: "Gamedev Roulette",
+    description: "Spin wheels. Make games. Get prizes.",
+    images: ["/opengraph-image.png"],
+  },
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-    <head>
-      <meta charSet="utf-8" />
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
 
-      <title>Gamedev Roulette</title>
-      <meta name="description" content="Spin Wheels. Make games. Get prizes." />
-
-      <meta property="og:url" content="https://roulette.hackclub.com" />
-      <meta property="og:type" content="website" />
-      <meta property="og:image" content="/opengraph-image.png" />
-
-      <meta name="twitter:card" content="summary_large_image" />
-      <meta property="twitter:domain" content="roulette.hackclub.com" />
-      <meta property="twitter:url" content="https://roulette.hackclub.com" />
-      <meta name="twitter:title" content="Gamedev Roulette" />
-      <meta name="twitter:description" contents="Spin Wheels. Make games. Get prizes." />
-      <meta name="twitter:image" content="/opengraph-image.png" />
-
-      <meta name="theme-color" content="#FF698A" />
-
-      <link rel="canonical" href="https://roulette.hackclub.com" />
-    </head>
       <body className={`${righteous.variable}`}>
         {children}
       </body>
